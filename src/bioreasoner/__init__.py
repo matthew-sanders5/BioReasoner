e79fd9c8b53c694b1aa737d5816240c189e1a14d
matthew-sanders5 <matthew.33.sand@gmail.com>
2026-01-14 14:20:02 -0500

    Expose package version

--- src/bioreasoner/__init__.py
+++ src/bioreasoner/__init__.py
@@ -4,7 +4,10 @@ from .engine import ReasoningEngine, ReasoningResult, ReasoningStep
 from .scenarios import Scenario, load_scenario
 from . import vocab
 
+__version__ = "0.1.0"
+
 __all__ = [
+    "__version__"
     "FactSet",
     "Rule",
     "build_default_wnt_rules",
